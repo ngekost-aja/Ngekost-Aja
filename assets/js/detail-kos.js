5c92b06f3ac855578163dac388c26487c9309698
Farrel Augusta Dinata <140877757+FarrelAD@users.noreply.github.com>
2024-07-05 16:05:22 +0700

    refactor navbar to use existing template & add new icon to some option (#7)

---
+++ assets/js/detail-kos.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function fetchTemplate(url) {
+        return fetch(url)
+            .then(response => response.text())
+            .then(templateHTML => {
+                const temp = document.createElement('div');
+                temp.innerHTML = templateHTML.trim();
+                return temp.querySelector('template').content
+            })
+            .catch(error => console.error(`Error fetching template from ${url}:`, error));
+    }
+
+    Promise.all([
+        fetchTemplate('../../template/Navbar.html'),
+    ]).then(([Navbar]) => {
+        // Attach navbar to the main HTML
+        const header = document.querySelector('header');
+        const cloneNavbar = document.importNode(Navbar, true);
+        cloneNavbar.querySelector('#homepage-link').href = '../index.html'
+        cloneNavbar.querySelector('#logo-on-navbar').src = '../assets/img/ngekost-aja-long-low.png'
+        cloneNavbar.querySelector('#search-submit-btn').onclick = 'window.location.href="search.html"'
+        header.appendChild(cloneNavbar);
+    })
+})
--- assets/js/pengajuan.js
+++ assets/js/pengajuan.js
@@ -12,6 +12,39 @@ const processStatus = document.getElementById('process-status')
 const processStatusText = document.getElementById('process-status-text')
 const statusIcon = document.getElementById('status-icon')
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    function fetchTemplate(url) {
+        return fetch(url)
+            .then(response => response.text())
+            .then(templateHTML => {
+                const temp = document.createElement('div');
+                temp.innerHTML = templateHTML.trim();
+                return temp.querySelector('template').content
+            })
+            .catch(error => console.error(`Error fetching template from ${url}:`, error));
+    }
+
+    Promise.all([
+        fetchTemplate('../../template/Navbar.html'),
+    ]).then(([Navbar]) => {
+        // Attach navbar to the main HTML
+        const header = document.querySelector('header');
+        const cloneNavbar = document.importNode(Navbar, true);
+        cloneNavbar.querySelector('#homepage-link').href = '../index.html'
+        cloneNavbar.querySelector('#logo-on-navbar').src = '../assets/img/ngekost-aja-long-low.png'
+
+        function goToSearchPage() {
+            window.location.href = 'search.html'
+        }
+
+        cloneNavbar.querySelector('#search-submit-btn').onclick = goToSearchPage() // This do not work
+        header.appendChild(cloneNavbar);
+    })
+})
+
+
+
 let currentStage = 1
 
 btnContinue.addEventListener('click', () => {
