3de42d48cd9db86ab2e1a88968da5a24d976a5e7
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-11-19 16:58:38 +0700

    remove cors configuration in the backend

--- backend/app/routes/api.php
+++ backend/app/routes/api.php
@@ -2,12 +2,8 @@
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type");
-
-use NgekostAjaBackend\Core\Router;
-use NgekostAjaBackend\Controllers\Kost;
+use App\Core\Router;
+use App\Controllers\Kost;
 
 
 Router::add('GET','/api/kost', Kost::class,'getSomeKostData');
