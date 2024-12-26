1610512cdc547d86b45c58fc19fb955ab13f495f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-26 14:56:41 +0700

    add logic to serve data single kost and add some try-catch block

--- backend/controllers/kost.controller.js
+++ backend/controllers/kost.controller.js
@@ -1,9 +1,32 @@
-import { getAllKostData } from "../repository/KostRepository.js"
+import { getAllKostData, getSingleKostData } from '../repository/KostRepository.js'
 
 
 const getAllKost = async (req, res) => {
-    const data = await getAllKostData()
-    res.json(data)
+    let data = null
+    try {
+        data = await getAllKostData()
+    } catch (error) {
+        return res.status(404).json({
+            status: "error",
+            message: "data not found"
+        })
+    }
+
+    res.status(200).json(data)
 }
 
-export { getAllKost }
+const getSingleKost = async (req, res) => {
+    let data = null
+    try {
+        data = await getSingleKostData(req.params.id)
+    } catch (error) {
+        return res.status(404).json({
+            status: "error",
+            message: "data not found"
+        })
+    }
+
+    res.status(200).json(data)
+}
+
+export { getAllKost, getSingleKost }
--- backend/repository/KostRepository.js
+++ backend/repository/KostRepository.js
@@ -3,13 +3,32 @@ import connectionPool from "../core/Database.js"
 
 const getAllKostData = async () => {
     try {
-        const [rows, fields] = await connectionPool.execute(`
-            SELECT * FROM kost
-        `)
-        return rows
+        const [result] = await connectionPool
+                                .execute(`SELECT * FROM kost`)
+
+        return result
     } catch (error) {
         console.log(error)
     }
 }
 
-export { getAllKostData }
+const getSingleKostData = async (itemID) => {
+    try {
+        const [result] = await connectionPool
+                                .execute(`
+                                    SELECT * 
+                                    FROM kost 
+                                    WHERE kode_kost = ?
+                                `, [itemID])
+
+        if (result.length === 0) {
+            throw new Error("Data not found");
+        }
+
+        return result[0]
+    } catch (error) {
+        throw error
+    }
+}
+
+export { getAllKostData, getSingleKostData }
