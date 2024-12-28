8464f098df8f488ee3a1cd21828d768178bc76d5
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-28 15:31:16 +0700

    add: user auth check before access homepage

--- backend/controllers/general.controller.js
+++ backend/controllers/general.controller.js
@@ -1,16 +1,18 @@
 import { getAllKostData, getSingleKostData } from '../repository/KostRepository.js'
 
 const viewHomePage = async (req, res) => {
-    let data = null
+    let kostData = null
     try {
-        data = await getAllKostData()
+        kostData = await getAllKostData()
     } catch (error) {
         return res.status(500).json({
             message: "server error!"
         })
     }
 
-    res.render('general/index', { data: data})
+    const isUserLoggedIn = !!req.session.user
+
+    res.render('general/index', { kost: kostData, user: { loginStatus: isUserLoggedIn } })
 }
 
 const viewLoginPage = (req, res) => {
