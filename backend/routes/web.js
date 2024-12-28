cb3122b002a28c3e9edc99bb6b80f9354605c10f
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-12-28 15:31:42 +0700

    fix: url http request to login

--- backend/routes/web.js
+++ backend/routes/web.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { viewHomePage, viewLoginPage } from '../controllers/general.controller.js'
+import { userLogin } from '../controllers/user.controller.js'
 
 
 const router = express.Router()
 
 router.get('/', viewHomePage)
 router.get('/login', viewLoginPage)
+
+router.post('/user/login', userLogin)
 
 export default router
