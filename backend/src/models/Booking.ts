51bb3c2515c09c56b0dd292feaace7952f89fa04
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-11-03 17:56:11 +0700

    feat: Implement booking functionality with CRUD operations and in-memory storage

---
+++ backend/src/models/Booking.ts
@@ -0,0 +1,11 @@
+export default interface Booking {
+    id: number;
+    userId: number;
+    houseId: number;
+    startDate: string;
+    endDate: string;
+    status: string;
+    totalPrice: string;
+    createdAt: string;
+    updatedAt: string;
+}
