64a48b2f8672906b04e3e45b657b5f2aece8eea3
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-12-27 16:31:20 +0700

    change backend default port to `8000`

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8000,
   db: {
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
